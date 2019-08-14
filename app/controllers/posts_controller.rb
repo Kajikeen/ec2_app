30419215b29301bbe0a79f5c1765d6d704c8b7da
Kajikeen <kajikaji0829@gmail.com>
2019-08-15 08:12:24 +0900

    progate rails3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+    @id = params[:id]
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    redirect_to("/posts/index")
+    @post = Post.new(content: params[:content])
+    @post.save
   end
 end
