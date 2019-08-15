0f54c422f704535f63dd884044c0711262790678
Kajikeen <kajikaji0829@gmail.com>
2019-08-15 11:01:48 +0900

    progate rails4

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,4 +16,21 @@ class PostsController < ApplicationController
     @post = Post.new(content: params[:content])
     @post.save
   end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
+  end
 end
