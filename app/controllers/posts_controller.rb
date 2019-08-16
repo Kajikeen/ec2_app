8d235b198eaf1065fd9c9feb2418d7ecd3ea4b8e
Kajikeen <kajikaji0829@gmail.com>
2019-08-16 13:55:33 +0900

    login_system

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,14 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user
+  before_action :ensure_correct_user,{only: [:edit, :update, :destroy]}
+  
   def index
     @posts = Post.all.order(created_at: :desc)
   end
 
   def show
-    @id = params[:id]
     @post = Post.find_by(id: params[:id])
+    @user = @post.user
   end
 
   def new
@@ -13,9 +16,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(content: params[:content])
+    @post = Post.new(
+      content: params[:content],
+      title: params[:title],
+      user_id: @current_user.id
+    )
     if @post.save
-      flash[:notice] = "投稿してくれて・・・ヘペトナス！！"
+      flash[:notice] = "投稿しました"
       redirect_to("/posts/index")
       else
       render("/posts/new")
@@ -30,7 +37,7 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
     @post.content = params[:content]
     if @post.save
-      flash[:notice] = "変更しました！ムナンチョ！"
+      flash[:notice] = "変更しました"
       redirect_to("/posts/index")
     else
       render("/posts/edit")
@@ -40,7 +47,16 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
-      flash[:notice] = "削除しました！"
+      flash[:notice] = "削除しました"
     redirect_to("/posts/index")
+  end
+
+
+  def ensure_correct_user
+    @post = Post.find_by(id: params[:id])
+    if @post.user_id != @current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_085701) do
+ActiveRecord::Schema.define(version: 2019_08_16_030851) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_08_15_085701) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.string "password"
   end
 
 end
